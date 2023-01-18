916fb0f53497c74ba35f56c974732a837bc6e8fb
PechMV <Pechm607@gmail.com>
2023-01-17 23:11:29 -0600

    worked on user-controller

---
+++ controller/user-controller.js
@@ -0,0 +1,55 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    getAllUser(req, res) {
+        User.find({})
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+    .populate({
+        path: 'thoughts',
+        select: '-__v'
+    })
+    .populate({
+        path: 'friends',
+        select: '-__v'
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+    });
+},
+
+createUser({ body }, res) {
+    User.create(body)
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => res.json(err));
+},
+
+updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+},
+}
